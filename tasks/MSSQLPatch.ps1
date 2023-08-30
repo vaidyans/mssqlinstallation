6c7b7bfad71317fa0580c5cf516e7de4ae5512f1
vaidyans <106604119+vaidyans@users.noreply.github.com>
2023-08-30 21:36:53 +0530

    Update MSSQLPatch.ps1

--- tasks/MSSQLPatch.ps1
+++ tasks/MSSQLPatch.ps1
@@ -1,5 +1,5 @@
 #----------------------------------------Install SQL Patch-------------------------------------------------
-$filepath="C:\SQL-SW\SQLServer2019-KB5011644-x64.exe"
+$filepath="C:\SQL-SW\SQLServer2012-KB4025925-x64.exe"
 $Parms = " /quiet /IAcceptSQLServerLicenseTerms /Action=Patch /AllInstances"
 $Prms = $Parms.Split(" ")
 & "$filepath" $Prms | Out-Null
