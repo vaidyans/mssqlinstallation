ed970498ccb8361d25a1aa76f1a5474104c66419
vaidyans <106604119+vaidyans@users.noreply.github.com>
2023-10-27 17:10:11 +0530

    Update MSSQLPatch.ps1

--- tasks/MSSQLPatch.ps1
+++ tasks/MSSQLPatch.ps1
@@ -1,19 +1,19 @@
 #----------------------------------------Install SQL Patch-------------------------------------------------
-Install-Module -Name powershell-yaml -Force -Scope CurrentUser
+#Install-Module -Name powershell-yaml -Force -Scope CurrentUser
 
 # Import the powershell-yaml module
-Import-Module powershell-yaml
+#Import-Module powershell-yaml
 
 # Specify the path to your YAML file
-$yamlFile = "https://github.com/vaidyans/mssqlinstallation/blob/master/vars/basevars.yml"
+#$yamlFile = "https://github.com/vaidyans/mssqlinstallation/blob/master/vars/basevars.yml"
 
 # Parse the YAML file
-$yamlData = ConvertFrom-Yaml -Path $yamlFile
+#$yamlData = ConvertFrom-Yaml -Path $yamlFile
 
 # Access the values from the YAML data
 
 #----- $filepath="C:\SQL-SW\SQLServer2012-KB4025925-x64.exe"
-$filepath = $yamlData.mssql_patch_file_name
+$filepath = mssql_patch_file_name
 
 $Parms = " /quiet /IAcceptSQLServerLicenseTerms /Action=Patch /AllInstances"
 $Prms = $Parms.Split(" ")
